0f96cf5b86490564d4500798fa76520fcd250818
jeangathoni8 <jeangathoni39@gmail.com>
2024-03-28 15:35:43 +0200

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+// Code your solution here
+function findMatching(drivers, name) {
+
+    const lowerCaseName = name.toLowerCase();
+    
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+    
+    return matchingDrivers;
+}
+
+module.exports = findMatching;
+
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+module.exports = matchName;
+
+
